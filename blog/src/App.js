2c789def5d7497ee54bfea9e021f8779309bbfcc
jsy8315 <tdhitbtd@gmail.com>
2024-06-11 20:22:06 +0900

    Part01.04.중요한 데이터는 변수말고 state에 담습니다_let [a, b] = useStae(['1', '2', '3'])_강의_과제

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,18 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
 
   // 서버에서 가져왔다고 가정
   let post = "광명 우동 맛집";
- 
+  let [글제목, b] = useState(['남자 코트 추천', "광명 우동 맛집", "파이도터 독학"])
+  let [logo, setLogo] = useState('ReactBlog')
+
   return (
     <div className="App">
       <div className='black-nav'>
-        <h4 id={post} style={{color : 'red', fontSize : '40px'}}>놀랍게도 블로그임</h4>
+        <h4 id={post} style={{color : 'yellow', fontSize : '40px'}}>{logo}</h4>
         {/* <h4>블로그임</h4> -> 실제 서비스였으면 서버 같은데서 블로그 글 가져와서 보여줄듯 */}
       </div>
-      <h4>{ post }</h4>
+      <div className='list'>
+        <h4>{글제목[0]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className='list'>
+        <h4>{글제목[1]}</h4>
+        <p>2월 18일 발행</p>
+      </div>
+      <div className='list'>
+        <h4>{글제목[2]}</h4>
+        <p>2월 19일 발행</p>
+      </div>
     </div>
   );
 }
