753b007edce75ff0075f970f755d57e4bac3485e
jsy8315 <tdhitbtd@gmail.com>
2024-06-11 19:56:56 +0900

    Part01.03.리액트에서 레이아웃 만들 때 쓰는 JSX 문법 3개_style = {{color : red}} 이런식으로 넣음

--- blog/src/App.js
+++ blog/src/App.js
@@ -5,11 +5,11 @@ function App() {
 
   // 서버에서 가져왔다고 가정
   let post = "광명 우동 맛집";
-
+ 
   return (
     <div className="App">
       <div className='black-nav'>
-        <h4 id={post}>놀랍게도 블로그임</h4>
+        <h4 id={post} style={{color : 'red', fontSize : '40px'}}>놀랍게도 블로그임</h4>
         {/* <h4>블로그임</h4> -> 실제 서비스였으면 서버 같은데서 블로그 글 가져와서 보여줄듯 */}
       </div>
       <h4>{ post }</h4>
