d4857d20b49968d3f474a92292b09f9d28a778e5
jsy8315 <tdhitbtd@gmail.com>
2024-06-21 07:45:44 +0900

    Part02.01.새로운 프로젝트 생성 & Bootstrap 사용하기

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button , Container , Nav , Navbar , NavDropdown} from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <Navbar bg="primary" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">H9</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">홈</Nav.Link>
+            <Nav.Link href="#features">특징</Nav.Link>
+            <Nav.Link href="#pricing">가격</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
     </div>
   );
 }
