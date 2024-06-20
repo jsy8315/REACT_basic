c561b807f23cad12af59d4a7ce5e39704bf578c1
jsy8315 <tdhitbtd@gmail.com>
2024-06-21 08:19:20 +0900

    Part02.02.이미지 넣는 법 & public 폴더 이용하기_html이미지넣기&상품목록레이아웃만들기

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import { Button , Container , Nav , Navbar , NavDropdown} from 'react-bootstrap';
+import { Button , Container , Nav , Navbar , Row, Col} from 'react-bootstrap';
+//import bg from './img/bg.png'; 귀찮으니까 걍 css쓸게용ㅋㅋ
 
 function App() {
   return (
     <div className="App">
     <>
-      <Navbar bg="primary" data-bs-theme="dark">
+      <Navbar bg="light" data-bs-theme="light">
         <Container>
           <Navbar.Brand href="#home">H9</Navbar.Brand>
           <Nav className="me-auto">
@@ -16,6 +17,30 @@ function App() {
           </Nav>
         </Container>
       </Navbar>
+
+      <div className='main-bg'></div>
+
+      <div>
+        <Container>
+          <Row>
+            <Col sm>
+              <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="80%"/>
+              <h4>상품명</h4>
+              <p>상품설명</p>
+            </Col>
+            <Col sm>
+            <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="80%"/>
+              <h4>상품명</h4>
+              <p>상품설명</p>
+            </Col>
+            <Col sm>
+            <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="80%"/>
+              <h4>상품명</h4>
+              <p>상품설명</p>
+            </Col>
+          </Row>
+        </Container>
+      </div>
     </>
     </div>
   );
