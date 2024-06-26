92244a755fead6f81d149aca8ea23e0f53a1c845
jsy8315 <tdhitbtd@gmail.com>
2024-06-26 11:59:00 +0900

    Part02.07.리액트 라우터 3 : URL 파라미터로 상세페이지 100개 만들기_useParams사용하여 데이터 가져오기

--- shop/src/routes/DetailPage.js
+++ shop/src/routes/DetailPage.js
@@ -2,24 +2,33 @@ import { useParams } from "react-router-dom"
 
 export default function DetailPage(props) {
 
-    let {id} = useParams();
-    console.log(id);
-
+    let {idUsingParams} = useParams();
+    console.log(idUsingParams);
+    
+    if (idUsingParams == props.shoes[idUsingParams].id) {
     return (
       <div className="container">
         <div className="row">
           <div className="col-md-6">
-            <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
+            <img src={`https://codingapple1.github.io/shop/shoes${props.shoes[idUsingParams].id + 1}.jpg`} width="100%" />
           </div>
           <div className="col-md-6">
-          <h4 className="pt-5">{props.shoes[id].title}</h4>
-          <p>{props.shoes[id].content}</p>
-          <p>{props.shoes[id].price}</p>
-          <button className="btn btn-danger">주문하기</button> 
-      </div>
-    </div>
-  </div> 
+            <h4 className="pt-5">{props.shoes[idUsingParams].title}</h4>
+            <p>{props.shoes[idUsingParams].content}</p>
+            <p>{props.shoes[idUsingParams].price}</p>
+            <button className="btn btn-danger">주문하기</button> 
+          </div>
+        </div>
+      </div> 
     )
+    }
+    else {
+      return (
+        <div>
+          <h2>잘못오셨어요</h2>
+        </div>
+      )
+    }
   }
 
 //   export default DetailPage; 위에 export 안쓰고 아래에 이렇게 써도 됨
