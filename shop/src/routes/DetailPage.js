7f03446f7c00e21ac5ff4b9d7be270e56c9222c9
jsy8315 <tdhitbtd@gmail.com>
2024-08-02 04:55:09 +0900

    Part02.09.Lifecycle과 useEffect 2_강의_setTimeout수정25

--- shop/src/routes/DetailPage.js
+++ shop/src/routes/DetailPage.js
@@ -33,11 +33,6 @@ export default function DetailPage(props) {
     useEffect(()=>{
       if (isNaN(num) == true){
         alert('그러지마라01')
-        alert('그러지마라01')
-        alert('그러지마라01')
-
-
-
       }
     }, [num])
   
