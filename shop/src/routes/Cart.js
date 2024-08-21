f82d1409ef778a6c6b8c3023ced5886654cfa8f6
jsy8315 <tdhitbtd@gmail.com>
2024-08-21 15:28:31 +0900

    02.17. store에 state 보관하기 & 데이터 store 보관 및 장바구니 데이터바인딩 과제

--- shop/src/routes/Cart.js
+++ shop/src/routes/Cart.js
@@ -1,8 +1,20 @@
 import {Table} from 'react-bootstrap' 
+import { useSelector } from 'react-redux'
 
 
 export default function Cart(){
-    
+
+    let a = useSelector((state)=>{ return state });
+    console.log(a.cartSample[0].id);
+    console.log(a.cartSample[0].name);
+
+
+    // 이런 형식의 데이터였음
+    // [
+    //     {id : 0, name : 'White and Black', count : 2},
+    //     {id : 2, name : 'Grey Yordan', count : 1}
+    //   ] 
+
     return(
         <div>
             <Table>
@@ -14,15 +26,26 @@ export default function Cart(){
                     <th>변경하기</th>
                   </tr>
                 </thead>
-                <tbody>
-                  <tr>
-                    <td>1</td>
-                    <td>안녕</td>
-                    <td>안녕</td>
-                    <td>안녕</td>
-                  </tr>
-                </tbody>
+                {
+                a.cartSample.map(function(a02, i){
+                    return(<CartStock  a02={a02} i={i}/>)
+                })
+                }
             </Table> 
         </div>
     )
+
+    function CartStock(props){
+        console.log(props)
+        return (
+            <tbody>
+            <tr>
+                <td>{props.a02.id}</td>
+                <td>{props.a02.name}</td>
+                <td>{props.a02.count}</td>
+                <td></td>
+            </tr>
+        </tbody>
+        )
+    }
 }
